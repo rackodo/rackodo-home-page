dc33832e5d086f011a5e8d49877b04635de8bd54
Bash Elliott <spicethings9@gmail.com>
2022-04-27 23:29:54 +1000

    more

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/Navbar.css'
 import '../styles/Headpanel.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
