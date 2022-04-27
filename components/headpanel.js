15b2dc8b084bf15aa7edb2ac41a89a48f0cb87ab
Bash Elliott <spicethings9@gmail.com>
2022-04-27 23:28:15 +1000

    designing is fun

--- components/headpanel.js
+++ components/headpanel.js
@@ -1,13 +1,20 @@
 import Image from 'next/image'
 
-const HeadPanel = ({title, subtitle, bg}) => {
+const HeadPanel = () => {
     const styling = {
-        backgroundImage: `url('${bg}')`,
     }
     return (
-        <div className="headpanel-container" style={styling}>
-            <h1>{title}</h1>
-            <h3>{subtitle}</h3>
+        <div className="headpanel" style={styling}>
+            <div className="headpanel-container">
+                <div className="disc">
+                    <img src="/images/slide.JPG" />
+                </div>
+                <div className="text">
+                    <h1>Bash Elliott</h1>
+                    <h3>Hobbyist Frontend and Python Developer based in Sydney, Australia (They/Them)</h3>
+                    <p>Bash is a soon-to-be highschool graduate with a passion for computing, robotics, software development and website design. They began their coding journey in early 2012 when they learned the basics of HTML from a book borrowed from their local library, and began teaching themselves how to design basic websites with the use of CSS. Since then, Bash has tried their hand at a variety of challenges in the programming field, including game development and app development, and even practices some sys-admin knowledge with their two servers at home.</p>
+                </div>
+            </div>
         </div>
     )
 }
--- components/navbar.js
+++ components/navbar.js
@@ -1,32 +1,34 @@
 /* Write a navbar Next component */
 
-import Image from 'next/image'
+import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 
-const Navbar = ({mode}) => {
+const Navbar = () => {
   return (
-    <div className="navbar ${mode}">
+    <div className="navbar">
+      <Script src="//pull.cappuccicons.com/cpf.js" />
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link href="/">
             <a>
               <img
                 className="navbar-logo-image"
-                src="/favicon.ico"
+                src="/images/logo.png"
                 alt="Logo"
               />
             </a>
           </Link>
         </div>
         <div className="navbar-links">
-          <Link href="/works">
-            <a>My Works</a>
+          <Link href="https://www.youtube.com/channel/UChiRebGN9a1oSoG_QZj6LjQ">
+            <a><i class="cp cp-youtube"></i></a>
           </Link>
-          <Link href="/about">
-            <a>About Me</a>
+          <Link href="https://github.com/raccter">
+            <a><i class="cp cp-github-o"></i></a>
           </Link>
-          <Link href="/contact">
-            <a>Contact Info</a>
+          <Link href="https://www.instagram.com/bash_elliott/">
+            <a><i class="cp cp-instagram-o"></i></a>
           </Link>
         </div>
       </div>
