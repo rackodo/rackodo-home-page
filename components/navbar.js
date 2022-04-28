dc853286266ee6bcbdb1edc7ced1b02a777d56ef
Bash Elliott <spicethings9@gmail.com>
2022-04-28 14:33:33 +1000

    changes to navbar
    modified navbar look and separated the navbar portions into separate files

--- components/navbar.js
+++ components/navbar.js
@@ -1,43 +1,63 @@
 /* Write a navbar Next component */
 
-import Head from 'next/head'
 import Link from 'next/link'
 import Script from 'next/script'
 import Image from 'next/image'
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      <Script src="//pull.cappuccicons.com/cpf.js" />
-      <div className="navbar-container">
-        <div className="navbar-logo">
-          <Link href="/">
-            <a>
-              <Image
-                className="navbar-logo-image"
-                src="/images/logo.png"
-                alt="Logo"
-                width={70}
-                height={70}
-                layout="raw"
-              />
-            </a>
-          </Link>
-        </div>
-        <div className="navbar-links">
-          <Link href="https://www.youtube.com/channel/UChiRebGN9a1oSoG_QZj6LjQ">
-            <a><i className="cp cp-youtube"></i></a>
-          </Link>
-          <Link href="https://github.com/raccter">
-            <a><i className="cp cp-github-o"></i></a>
-          </Link>
-          <Link href="https://www.instagram.com/bash_elliott/">
-            <a><i className="cp cp-instagram-o"></i></a>
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="navbar">
+			<Script src="//pull.cappuccicons.com/cpf.js" />
+			<div className="navbar-container">
+				<NavbarLogo />
+				<NavbarLinks>
+					<NavbarLink 
+					href="https://www.youtube.com/channel/UChiRebGN9a1oSoG_QZj6LjQ" 
+					icon="cp cp-youtube" />
+					<NavbarLink 
+					href="https://github.com/raccter" 
+					icon="cp cp-github" />
+					<NavbarLink 
+					href="https://www.instagram.com/bash_elliott/" 
+					icon="cp cp-instagram" />
+				</NavbarLinks>
+			</div>
+		</div>
+	)
+}
+
+const NavbarLogo = () => {
+	return (
+		<Link href="/">
+			<a>
+				<Image
+					className="navbar-logo-image"
+					src="/images/logo.png"
+					alt="Logo"
+					width={70}
+					height={70}
+					layout="raw"
+				/>
+			</a>
+		</Link>
+	)
+}
+
+const NavbarLink = ({href, icon}) => {
+	return (
+		// eslint-disable-next-line @next/next/link-passhref
+		<Link href={href}>
+			<div className="cell"><a href={href}><i className={icon}></i></a></div>
+		</Link>
+	)
+}
+
+const NavbarLinks = ({children}) => {
+	return (
+		<div className="navbar-links">
+			{children}
+		</div>
+	)
 }
 
 export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar'
 import HeadPanel from '../components/headpanel'
 import HeadInfo from '../components/headinfo'
@@ -10,7 +7,7 @@ export default function Home() {
     <div>
       <HeadInfo title="Bash Elliott" />
       <HeadPanel />
-      <Navbar/>
+      <Navbar />
     </div>
   )
 }
