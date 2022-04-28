f52a519bf3eea71c77217c33109aed6157631773
Bash Elliott <spicethings9@gmail.com>
2022-04-28 14:51:45 +1000

    tbc when i can get a phone to debug with

--- components/headinfo.js
+++ components/headinfo.js
@@ -11,7 +11,8 @@ const HeadInfo = ({title}) => {
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+            <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
             
             {/* Primary Meta Tags */}
             <meta name="title" content="Bash Elliott" />
