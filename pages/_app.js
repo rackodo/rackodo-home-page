e166c16cffe8bc62e7495083522179ec7a9e74ef
Bash Elliott <spicethings9@gmail.com>
2022-04-29 13:49:22 +1000

    easy money

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/Navbar.css'
 import '../styles/Headpanel.css'
 import '../styles/Info.css'
 import '../styles/Works.css'
+import '../styles/Skills.css'
 import '../styles/Footer.css'
 
 function MyApp({ Component, pageProps }) {
