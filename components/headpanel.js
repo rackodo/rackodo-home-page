f1e538b0e47f696d478a8ca76522aa4b70dfbf9f
Bash Elliott <spicethings9@gmail.com>
2022-06-13 19:39:39 +1000

    fixed

--- components/headpanel.js
+++ components/headpanel.js
@@ -19,19 +19,19 @@ const HeadPanel = () => {
     })
 
     return (
-        <div className="headpanel">
+        <div className="headpanel" >
             <div className="video-container">
                 <video autoPlay muted loop>
                     <source src="/headpanel.mp4" type="video/mp4"/>
                 </video>
             </div>
-            <div className={`headpanel-container ${isVisible ? "panelVisible" : "panelNotVisible"}`}>
+            <div ref={containerRef} className={`headpanel-container ${isVisible ? "panelVisible" : "panelNotVisible"}`}>
                 <div className="headpanel-text">
-                    <h1 ref={containerRef}>Bash Elliott</h1>
+                    <h1 >Bash Elliott</h1>
                     <h3>Hobbyist Frontend and Python Developer based in Sydney, Australia</h3>
                     {/* <p>Bash is a soon-to-be highschool graduate with a passion for computing, robotics, software development and website design. They began their coding journey in early 2012 when they learned the basics of HTML from a book borrowed from their local library, and began teaching themselves how to design basic websites with the use of CSS. Since then, Bash has tried their hand at a variety of challenges in the programming field, including game development and app development, and even practices some sys-admin knowledge with their two servers at home.</p> */}
                 </div>
-                <div className='headpanel-buttons'>
+                <div className='headpanel-buttons' >
                     <HeadPanelButton label="Contact" href="mailto:spicethings9@gmail.com" />
                     <HeadPanelButton label="Learn More" href="#info" onClick={scrollToInfo} />
                 </div>
