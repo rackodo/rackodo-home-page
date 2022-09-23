897b29182b294e80fa1b16f3a9aeb14bb97ca8b6
Bash Elliott <bashelliott@gmail.com>
2022-09-23 18:58:41 +1000

    push to main

--- pages/404.js
+++ pages/404.js
@@ -7,16 +7,10 @@ import Head from 'next/head'
 
 export default function FourOhFour() {
 	return(
-		<>
-			<Head>
-				<title>Bash Elliott&apos;s... wait what?</title>
-			</Head>
-			<div className={styles.wrapper}>
-				<Navbar />
-				<div className={styles.content}>
+		<div>
+			<div className={styles.content}>
 					<Callout>Oop! That&apos;s not a real page.<br/><InlineLink href="/">Let&apos;s get back to normal.</InlineLink></Callout>
-				</div>
 			</div>
-		</>
+		</div>
 	)
 }
