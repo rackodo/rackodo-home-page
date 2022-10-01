e43f168d5481a1b9c6d979997d3241358c5370ca
Bash Elliott <bashelliott@gmail.com>
2022-10-01 14:27:38 +1000

    completed portfolio, ready for theming

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
 					Bash is a soon-to-be high school graduate with a passion for computing, robotics, software development and website design. They&apos;ve tried their hand at a variety of challenges in the programming field, including game development and app development, and even practices some sys-admin knowledge with their two servers at home. When not online, they enjoy watching films and exploring their neighbourhood.
 				</ParaSection>
 				<ParaSection>
-					Bash also practices creative writing in their spare time and occasionally makes videos for their YouTube channel <Link color={"blue.600"} href="https://www.youtube.com/channel/UChiRebGN9a1oSoG_QZj6LjQ">Rackodo</Link>. They also enjoy playing guitar recreationally.
+					Bash also practices creative writing in their spare time and occasionally makes videos for their YouTube channel <Link color={"blue.600"} href="https://www.youtube.com/channel/UChiRebGN9a1oSoG_QZj6LjQ" isExternal>Rackodo</Link>. They also enjoy playing guitar recreationally.
 				</ParaSection>
 				<Container p={0} textAlign="center">
 					<Button
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Section, { ParaSection } from "../components/section";
 import { H1 } from "../components/headings";
+import Gallery, { GalleryItem } from "../components/gallery";
+import { Link } from "@chakra-ui/react";
 
 export default function Portfolio() {
 	return(
 		<Section>
 			<H1>Portfolio</H1>
-			<ParaSection>
-				Portfolio here.
-			</ParaSection>
+			<Gallery>
+				<GalleryItem name="Commissions Page" href="https://github.com/rackodo/euphoria-comms" src="/images/project-logos/euphoria-comms-page.png">
+					Commissions page for my friend, <Link color={"blue.600"} href="https://twitter.com/you_go_doe" isExternal>Euphoria</Link>. Uses a column-based design for dividing information into separate sections that can be switched between using the available buttons.
+				</GalleryItem>
+				<GalleryItem name="Login Page Design" href="https://github.com/rackodo/login-page" src="/images/project-logos/login-page.png">
+					Login page design that uses dynamic CSS to show when valid information is inside each input field, and when the form is valid overall.
+				</GalleryItem>
+				<GalleryItem name="Card Page Design" href="https://github.com/rackodo/card-page" src="/images/project-logos/card-page.png">
+					Simple card page design that contains a brief summary of a person and their social media links. Includes a hue slider that dynamically updates the colours of the card.
+				</GalleryItem>
+			</Gallery>
 		</Section>
 	)
 }
