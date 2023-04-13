742c62ae80926a38d6c037b17832e3435bf9d48e
Dustin Kroeger <dustin.kroeger@telekom.de>
2023-04-13 14:10:31 +0200

    extend if you want :3

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,11 @@
 import { Box, Circle, Container, Flex, HStack, Link } from "@chakra-ui/react";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+
+import { useRouter } from "next/router";
+import NextLink from 'next/link';
+
 
 export default function Navbar() {
-  const home = useRef(null);
-  const resume = useRef(null);
-  const portfolio = useRef(null);
-
-  const [active, setActive] = useState(home);
-
-  function handleActive(e, item) {
-	console.log(e)
-  }
-
-  useEffect(() => {
-    console.log(home);
-    home.current.active = (home === active);
-    resume.current.active = (resume === active);
-    portfolio.current.active = (portfolio === active);
-  }, [active]);
 
   return (
     <HStack
@@ -28,54 +16,52 @@ export default function Navbar() {
       gap="2.5px"
       paddingX="5px"
     >
-      <NavLink
-        name="Home"
-        href="/"
-        id="home"
-        ref={home}
-		data-active={false}
-        onClick={(val) => handleActive(val.target)} active={false}
-      />
-      <NavLink
-        name="Resume"
-        href="/resume"
-        id="resume"
-        ref={resume}
-		data-active={false}
-        onClick={(val) => handleActive(val.target)} active={true}
-      />
-      <NavLink
-        name="Portfolio"
-        href="/portfolio"
-        id="portfolio"
-        ref={portfolio}
-		data-active={false}
-        onClick={(val) => {handleActive(val.target)}} active={false}
-      />
-      <NavLink
-        name="Contact Me"
-        href="mailto:rackodo.business@gmail.com"
-      />
-    </HStack>
+      <NextLink href="/" passHref legacyBehavior>
+        <NavLink
+          name="Home"
+          id="home"
+        />
+      </NextLink>
+      <NextLink href="/resume" passHref legacyBehavior>
+        <NavLink
+          name="Resume"
+          id="resume"
+        />
+      </NextLink>
+      <NextLink href="/portfolio" passHref legacyBehavior>
+        <NavLink
+          name="Portfolio"
+          id="portfolio"
+        />
+      </NextLink>
+      <NextLink href="mailto:rackodo.business@gmail.com" passHref legacyBehavior>
+        <NavLink
+          name="Contact Me"
+        />
+      </NextLink>
+    </HStack >
   );
 }
 
-const NavLink = React.forwardRef((props, ref) => (
-  <Circle
-    as={"button"}
-    ref={ref}
-    textDecoration="None!important"
-    color="initial"
-    height="40px"
-    flex={props.active ? "100%" : "none"}
-    bg={props.active ? "red.200" : "initial"}
-    alignItems="center"
-    justifyContent="center"
-    onClick={props.onClick}
-    p={0}
-  >
-    {props.name}
-  </Circle>
-));
+const NavLink = React.forwardRef((props, ref) => {
+  const router = useRouter();
+  return (
+    <Circle
+      as={"button"}
+      ref={ref}
+      textDecoration="None!important"
+      color="initial"
+      height="40px"
+      flex={props.href == router.pathname ? "100%" : "none"}
+      bg={props.href == router.pathname ? "red.200" : "initial"}
+      alignItems="center"
+      justifyContent="center"
+      onClick={props.onClick}
+      p={0}
+    >
+      {props.name}
+    </Circle>
+  )
+});
 
 NavLink.displayName = "NavLink";
