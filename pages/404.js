924390aebbb0666e2aa6730fc1ffebf65ff5302f
Bash Elliott <bashelliott@gmail.com>
2024-01-22 14:15:47 +1100

    dummy page

--- pages/404.js
+++
@@ -1,7 +0,0 @@
-import Callout from "../components/callout"
-
-export default function fourohfour() {
-	return(
-		<Callout>This page doesn&apos;t exist.</Callout>
-	)
-}
--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,12 @@
-import Head from 'next/head'
-import { ChakraProvider } from '@chakra-ui/react'
-import Main from '../layouts/main'
-import theme from '../theme.ts'
-import { Analytics } from '@vercel/analytics/react'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
-	return (
-		<ChakraProvider theme={theme}>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				{/* Primary Meta Tags */}
-				<title>Bash Elliott</title>
-				<meta name="title" content="Bash Elliott" />
-				<meta name="description" content='Official website of Bash "Rackodo" Elliott - Frontend Website Developer based in Sydney, Australia.' />
-
-				{/* Open Graph / Facebook */}
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://rackodo.vercel.app/" />
-				<meta property="og:title" content="Bash Elliott" />
-				<meta property="og:description" content='Official website of Bash "Rackodo" Elliott - Frontend Website Developer based in Sydney, Australia.' />
-				<meta property="og:image" content="/images/station.JPG" />
-
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://rackodo.vercel.app/" />
-				<meta property="twitter:title" content="Bash Elliott" />
-				<meta property="twitter:description" content='Official website of Bash "Rackodo" Elliott - Frontend Website Developer based in Sydney, Australia.' />
-				<meta property="twitter:image" content="/images/station.JPG" />
-			</Head>
-			<Main>
-				<Component {...pageProps}/>
-				<Analytics/>
-			</Main>
+function App({ Component, pageProps }) {
+	return(
+		<ChakraProvider>
+			<Component {...pageProps} />
 		</ChakraProvider>
 	)
 }
 
-export default MyApp
+export default App
