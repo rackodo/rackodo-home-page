21d62bf970c0c678e2a1548e4017dbecdca78824
Bash Elliott <bashelliott@gmail.com>
2024-10-22 09:23:59 +1100

    push

---
+++ script.js
@@ -0,0 +1,14 @@
+let titles = [
+	"rackodo",
+]
+
+let titleCounter = 0;
+
+// setInterval(() => {
+// 	document.getElementById('header').innerHTML = titles[titleCounter]
+// 	titleCounter++;
+
+// 	if (titleCounter > titles.length - 1) {
+// 		titleCounter = 0;
+// 	}
+// }, 500);
